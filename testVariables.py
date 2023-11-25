bb045d593763ca08278cdd0f40fcde6d5b47529a
Riccardo De Simini <127334418+RiccardoD25@users.noreply.github.com>
2023-11-25 18:01:39 -0500

    Add files via upload

---
+++ testVariables.py
@@ -0,0 +1,7 @@
+def testVariable():
+    name=input("What is you name?")
+    print("Your name is",name)
+    age=int(input("how old are you?"))
+    print("you are",age+1,"years old.")
+    
+testVariable()
